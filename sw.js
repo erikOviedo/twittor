11a5ef949dbb74f2a818858393462d007ad31b91
erikOviedo <erik.oviedoe@gmail.com>
2021-03-10 12:36:25 -0600

    Modificamos rutas

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const INMUTABLE_CACHE = 'inmutable-v1';
 
 
 const APP_SHELL = [
-    'http://localhost/pwa/06-twittor/',
+    // '/',
     'index.html',
     'css/style.css',
     'img/favicon.ico',
